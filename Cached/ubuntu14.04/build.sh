5c41450e6908ff975129644795c90320b5b66937
h00226008 <huangjingkai@huawei.com>
2017-11-18 13:38:20 +0800

    build.sh

--- Cached/ubuntu14.04/build.sh
+++ Cached/ubuntu14.04/build.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
+# Copyright @ HuangJingkai. All Rights Reserved.
+
 set -o errexit
-set -o nounset
 set -o pipefail
 
-if [ ! -d nginx ];then 
-	echo "The source attachment does not contain the foder: ./nginx/, download from github.com/huangjingkai/Mall please. "
-	exit 1
-fi
+[ -f /etc/apt/sources.list ] && mv sources.list /tmp
+find /etc/apt -type f | xargs rm -f
 
-[ -f /etc/apt/sources.list ] && mv /etc/apt/sources.list /etc/apt/sources.list_bak; 
 wget -O sources.list http://mirrors.163.com/.help/sources.list.trusty; 
-cp -f sources.list /etc/apt
+cp sources.list /etc/apt
 
 apt-get update;
-apt-get install -y nginx nginx-extras lua-nginx-redis unzip git redis-tools dos2unix;
+apt-get install -y --force-yes nginx nginx-extras lua-nginx-redis unzip git redis-tools dos2unix;
 
 [ ! -d /usr/share/nginx/demo_html ] && mkdir -p /usr/share/nginx/demo_html;
 chown -R www-data:www-data  /usr/share/nginx/demo_html;
 
 # unzip nginx.zip
-cp -r nginx/* /etc/nginx/
+cp -rf nginx/* /etc/nginx/
 
 nginx -s reload;
